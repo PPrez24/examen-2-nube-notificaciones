d1498040df3d3962a08c8053e4f8257cacaeed63
PPrez24 <eduardo.perez@iteso.mx>
2024-11-28 01:48:24 -0600

    prueba10

--- Dockerfile
+++ Dockerfile
@@ -13,11 +13,17 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Add build arguments for SNS credentials
 ARG AWS_REGION
 ARG SNS_TOPIC_ARN
+ARG AWS_ACCESS_KEY_ID
+ARG AWS_SECRET_ACCESS_KEY
+ARG AWS_SESSION_TOKEN
+
 
 # Set environment variables inside the container
 ENV AWS_REGION=$AWS_REGION
 ENV SNS_TOPIC_ARN=$SNS_TOPIC_ARN
-
+ENV AWS_ACCESS_KEY_ID
+ENV AWS_SECRET_ACCESS_KEY
+ENV AWS_SESSION_TOKEN
 # Expose the port
 EXPOSE 5002
 
--- app.py
+++ app.py
@@ -4,7 +4,12 @@ import os
 
 app = Flask(__name__)
 
-sns = boto3.client('sns', region_name=os.getenv("AWS_REGION"))
+sns = boto3.client('sns', 
+    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN")  # Solo si usas tokens temporales
+)
 
 @app.route('/notificaciones', methods=['POST'])
 def enviar_notificacion():
